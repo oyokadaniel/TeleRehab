de23c3c184a5985718bd63a37acdb0ee29d0a061
Daniel_Oyoka <oyokadaniel@gmail.com>
2023-08-09 03:44:19 +0000

    provided more design

--- lib/screens/program_screen.dart
+++ lib/screens/program_screen.dart
@@ -3,57 +3,78 @@ import 'package:telerehab/screens/moduledetails_screen.dart';
 
 class ProgramPage extends StatelessWidget {
   final List<String> modules = [
-    "Physio Therapy",
-    "Nutritional Therapy",
-    "Orthopeadic Technology",
-    "phycaitric Therapy",
+    "Physical Therapy",
     "Occupational Therapy",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Choose your Program'),
-      ),
-      body: ListView.builder(
-        itemCount: modules.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: GestureDetector(
-              onTap: () {
-                // Navigate to the module details page when the user taps on a module
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        ModuleDetailsPage(moduleName: modules[index]),
-                  ),
-                );
-              },
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                decoration: BoxDecoration(
-                  color: Color(0x79ffffff),
-                  borderRadius: BorderRadius.circular(8.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      spreadRadius: 2,
-                      blurRadius: 4,
-                      offset: Offset(0, 2),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "Select a Module",
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: modules.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              ModuleDetailsPage(moduleName: modules[index]),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 2,
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(20.0),
+                      margin: EdgeInsets.only(bottom: 20.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.folder,
+                            color: Colors.blue,
+                            size: 40.0,
+                          ),
+                          SizedBox(width: 20.0),
+                          Expanded(
+                            child: Text(
+                              modules[index],
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-                child: Text(
-                  modules[index],
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
+                  );
+                },
               ),
             ),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
